783672bc7abfcf356c5b399d82352858bbae3837
2005Andrei <67106225+2005Andrei@users.noreply.github.com>
2021-02-17 21:10:46 +0200

    improved exception handlers

--- improvedVerbs.py
+++ improvedVerbs.py
@@ -7,7 +7,7 @@ db = SQL("sqlite:///verbs.db")
 #cmd0 = input(">_ ")
 while True:
 	cmd0 = input(">_ ")
-	if cmd0 == ".help":
+	if cmd0 == "help":
 		print("This is a command line programm which is a cheat sheet for german classes.")
 		for i in range(50):
 			print("=", end='')
@@ -16,46 +16,56 @@ while True:
 		for i in range(50):
                         print("=", end='')
 		print()
-		print("To exit type: <.exit()>")
-	elif cmd0 == ".exit()":
+		print("To exit type: <exit>")
+	elif cmd0 == "exit":
 		sys.exit(1)
 	else:
 		try:
-        		str(cmd0)
-		except ValueError:
-        		print("Type a string mf")
-       			sys.exit(1)
-		verb = db.execute("select verb from verbs where verb like :verb", verb=cmd0)
-		v2 = db.execute("select v2 from verbs where verb like :verb", verb=cmd0)
-		v3 = db.execute("select v3 from verbs where verb like :verb", verb=cmd0)
-		online = db.execute("select book from verbs where verb like :verb", verb=cmd0)
-		trad = db.execute("select traducere from verbs where verb like :verb", verb=cmd0)
-		
-		if verb != None:
-        		print("\n")
-        		print("Verb        ", end='')
-        		print(" V2        ", end='')
-        		print(" V3        ", end='')
-        		print(" Online        ", end='')
-        		print(" Traducere       ")
-        		for i in range(60):
-                		print("=", end='')
-        		print("\n")
-        		print(verb[0]["verb"] + "   ", end='')
-        		print(v2[0]["v2"] + "   ", end='')
-        		print(v3[0]["v3"] + "   ", end='')
-        		print(online[0]["book"] + "   ", end='')
-        		print(trad[0]["traducere"])
-        		print("\n")
+			try:
+        			str(cmd0)
+			except ValueError:
+        			print("Type a string mf")
+       				sys.exit(1)
 
-		else: 
-			#Wrong spelling 
-			#I am going to improve this search alg eventually 
-			firstLetter = cmd0[0] 
-		#print("No matches found, I am going to give you a list of verbs starting wth that letter")
+			verb = db.execute("select verb from verbs where verb like :verb", verb=cmd0)
+			v2 = db.execute("select v2 from verbs where verb like :verb", verb=cmd0)
+			v3 = db.execute("select v3 from verbs where verb like :verb", verb=cmd0)
+			online = db.execute("select book from verbs where verb like :verb", verb=cmd0)
+			trad = db.execute("select traducere from verbs where verb like :verb", verb=cmd0)
+
+			print("\n")
+			print("Verb        ", end='')
+			print(" V2        ", end='')
+			print(" V3        ", end='')
+			print(" Online        ", end='')
+			print(" Traducere       ")
+			for i in range(60):
+				print("=", end='')
+			print("\n")
+			print(verb[0]["verb"] + "   ", end='')
+			print(v2[0]["v2"] + "   ", end='')
+			print(v3[0]["v3"] + "   ", end='')
+			print(online[0]["book"] + "   ", end='')
+			print(trad[0]["traducere"])
+			print("\n")
+		except RuntimeError:
+                        #Wrong spelling
+                     	#I am going to improve this search alg eventually
+			firstLetter = cmd0[0]
+			print("No matches found, I am going to give you a list of verbs starting wth that letter")
 			print()
-			verbs = db.execute("select verb from verbs where verb like ':verb%'", verb=cmd0[0])
+			verbs = db.execute("select verb from verbs where Left(verb, 1) = ':verb'", verb=cmd0[0])
 			
+			print("\n")
+			print("Verb        ", end='')
+			print(" V2        ", end='')
+			print(" V3        ", end='')
+			print(" Online        ", end='')
+			print(" Traducere       ")
+			for i in range(60):
+				print("=", end='')
+			print("\n")
+
 			for verb in verbs:
 				v2 = db.execute("select v2 from verbs where verb like :verb", verb=verb)
 				v3 = db.execute("select v3 from verbs where verb like :verb", verb=verb)
